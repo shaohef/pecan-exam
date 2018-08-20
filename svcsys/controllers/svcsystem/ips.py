baeb2849bb64b39e9283eb5c9727568b0dc935bf
Shaohe Feng <shaohe.feng@intel.com>
2018-08-19 21:52:04 -0400

    Support shell command call

--- svcsys/controllers/svcsystem/ips.py
+++ svcsys/controllers/svcsystem/ips.py
@@ -1,5 +1,20 @@
 import pecan
 from pecan import rest, response
+import subprocess
+
+
+def syscall(*args, **kwargs):
+    process = subprocess.Popen(*args, stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE, **kwargs)
+    output, err = process.communicate()
+    retcode = process.poll()
+    return retcode, output, err
+
+
+CMDS= [
+"echo 'Hello world, this is a test!'"
+]
+
 
 class SystemIPController(rest.RestController):
 
@@ -17,7 +32,12 @@ class SystemIPController(rest.RestController):
             response.status = 400
             missings = ", ".join(missing)
             return {'message': 'Missing these ips in the context: %s.' % missings}
+        for cmd in CMDS:
+            r, o, e = syscall(cmd, shell=True)
+            if r != 0:
+                response.status = 500
+                return {'message': e}
+            print o
         # TODO: Create a new order, (optional) return some status data
         response.status = 201
         return {'status': 'POST SUCCESS!\n'}
-
